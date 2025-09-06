544372f5e24df20f927d4c0f39d14a43f9c347a3
SvGaming <sven.kovac1@outlook.com>
2025-09-06 23:40:57 +0200

    switch to BigDecimal

--- src/main/java/org/betamc/calculator/Calculator.java
+++ src/main/java/org/betamc/calculator/Calculator.java
@@ -4,11 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Calculator extends JavaPlugin {
     public void onEnable() { getServer().getLogger().info("[Calculator] Plugin loaded."); }
     public void onDisable() {}
-    private static String trimTrailingZeros(float number) { return Float.toString(number).replaceAll("\\.?0*$", ""); }
+    private static String trimTrailingZeros(BigDecimal number) { return number.toString().replaceAll("\\.?0*$", ""); }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("calc") || label.equalsIgnoreCase("calculator")) {
@@ -17,12 +19,12 @@ public class Calculator extends JavaPlugin {
                 return false;
             }
 
-            float num1, num2, result;
+            BigDecimal num1, num2, result;
             String operator = args[1];
 
             try {
-                num1 = Float.parseFloat(args[0]);
-                num2 = Float.parseFloat(args[2]);
+                num1 = new BigDecimal(args[0]);
+                num2 = new BigDecimal(args[2]);
             }
             catch (NumberFormatException e) {
                 sender.sendMessage("§cError: Arguments must be valid numbers");
@@ -31,23 +33,29 @@ public class Calculator extends JavaPlugin {
 
             switch(operator) {
                 case "*":
-                    result = num1 * num2;
+                    result = num1.multiply(num2);
                     break;
                 case "/":
-                    result = num1 / num2;
+                    try {
+                        result = num1.divide(num2, 10, RoundingMode.HALF_UP);
+                    }
+                    catch (ArithmeticException e) {
+                        sender.sendMessage("§cError: Arithmetic exception (Division by 0?)");
+                        return false;
+                    }
                     break;
                 case "+":
-                    result = num1 + num2;
+                    result = num1.add(num2);
                     break;
                 case "-":
-                    result = num1 - num2;
+                    result = num1.subtract(num2);
                     break;
                 default:
                     sender.sendMessage("§cError: Invalid operator (must be +,-,*,/)");
                     return false;
             }
 
-            sender.sendMessage("Result of §a" + trimTrailingZeros(num1) + " §f" + operator + " §a" + trimTrailingZeros(num2) + "§f: §a" + trimTrailingZeros(result));
+            sender.sendMessage("Result of §a" + num1 + " §f" + operator + " §a" + num2 + "§f: §a" + trimTrailingZeros(result));
         }
 
         return true;
